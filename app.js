9dceb9733328c555c85de59b82f7c6224cebee59
jorgelinadev <jorgelinalberti@gmail.com>
2022-07-08 10:50:32 -0300

    First commit

---
+++ app.js
@@ -0,0 +1,9 @@
+//alert ("Mi archivo está bien linkeado")
+
+const nombre = prompt ('Ingresa tu nombre');
+const apellido = prompt ('Ingresa tu apellido');
+
+alert ('Hola ' +nombre +' '+ apellido+ ', bienvenid@ a ADA');
+
+console.log (alert);
+
